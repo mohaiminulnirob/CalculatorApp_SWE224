2cae21b3ea8ae3c11d8dd42ea2098d61a7cabde2
Nirob <mohaiminulislam20000@gmail.com>
2024-05-29 02:57:40 +0600

    Add CalculatorFrame

---
+++ src/Calculator/CalculatorFrame.java
@@ -0,0 +1,19 @@
+package Calculator;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class CalculatorFrame extends JFrame{
+    JFrame frame;
+    CalculatorFrame() {
+        frame = new JFrame();
+        frame.setTitle("Calculator");
+        frame.setSize(300, 490);
+        frame.getContentPane().setLayout(null);
+        frame.getContentPane().setBackground(Color.black);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
